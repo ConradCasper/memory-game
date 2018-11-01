b920b54a29fa68d7b4ab453bacc2a5a4788fab79
Conrad Casper <conradcasper@Conrads-MacBook-Pro.local>
2018-11-01 17:53:54 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
+var cards = ['queen' , 'queen' , 'king' , 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
+
+
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+console.log("User flipped " + cardTwo)
+
+
+
+
